e3f415f1ae2870b2c9308508f22fb9d681c9230f
Magdalen Berns <m.berns@thismagpie.com>
2015-07-03 18:31:02 +0100

    Fix unchecked operations in ExampleFileView

--- ExampleFileView.java
+++ ExampleFileView.java
@@ -68,9 +68,9 @@ import java.util.Hashtable;
  * @author Jeff Dinkins
  */
 public class ExampleFileView extends FileView {
-    private Hashtable icons = new Hashtable(5);
-    private Hashtable fileDescriptions = new Hashtable(5);
-    private Hashtable typeDescriptions = new Hashtable(5);
+    private Hashtable<String,Icon> icons              = new Hashtable<>(5);
+    private Hashtable<File,String> fileDescriptions   = new Hashtable<>(5);
+    private Hashtable<String,String> typeDescriptions = new Hashtable<>(5);
 
     /**
      * The name of the file.  Do nothing special here. Let
