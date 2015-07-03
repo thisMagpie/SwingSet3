d92977192cef57db31453881f0e100f32d7a557d
Magdalen Berns <m.berns@thismagpie.com>
2015-07-03 17:58:12 +0100

    Fix unchecked operations in CodeViewer

--- CodeViewer.java
+++ CodeViewer.java
@@ -78,7 +78,8 @@ import java.util.*;
  */
 public class CodeViewer {
 
-    private static HashMap reservedWords = new HashMap(); // >= Java2 only (also, not thread-safe)
+    private static HashMap<String,String>reservedWords = new HashMap<>(); // >= Java2 only (also,
+    // not thread-safe)
     //private static Hashtable reservedWords = new Hashtable(); // < Java2 (thread-safe)
     private boolean inMultiLineComment = false;
     private String backgroundColor = "#ffffff";
@@ -283,7 +284,8 @@ public class CodeViewer {
             return "";
         }
         StringBuffer buf = new StringBuffer();
-        HashMap usedReservedWords = new HashMap(); // >= Java2 only (not thread-safe)
+        HashMap<String,String> usedReservedWords = new HashMap<>(); // >= Java2 only (not
+        // thread-safe)
         //Hashtable usedReservedWords = new Hashtable(); // < Java2 (thread-safe)
         int i=0, startAt=0;
         char ch;
